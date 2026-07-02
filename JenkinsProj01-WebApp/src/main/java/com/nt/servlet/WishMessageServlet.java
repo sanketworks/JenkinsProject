803309386594738502c1f44b9429634406e49edb
sanket <mishra.sanket501@gmail.com>
2026-07-02 16:00:04 +0530

    initial commit

---
+++ JenkinsProj01-WebApp/src/main/java/com/nt/servlet/WishMessageServlet.java
@@ -0,0 +1,35 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/wishurl")
+public class WishMessageServlet extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//set the response content type
+		resp.setContentType("text/html");
+		//get PrintWriter
+		PrintWriter pw=resp.getWriter();
+		//write the message to response object
+		pw.println("<h1 style='color:red;text-align:center'>Good Morning</h1>");
+		//home hyperlink
+		pw.println("<br><br><a href='index.jsp'>home</a>");
+		//close the stream
+		pw.close();
+
+	}
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}
---
+++ JenkinsProj01-WebApp/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+
+
+<h1 style="color:red;text-align:center">Jenkins web application</h1>
+<a href="wishurl">show wish message</a>
+
