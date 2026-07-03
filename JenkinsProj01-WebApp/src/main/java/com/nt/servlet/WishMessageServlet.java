da2744f65718eacf7230e196e8fc13d55182f3b1
sanket <mishra.sanket501@gmail.com>
2026-07-03 18:47:21 +0530

    third commit

--- JenkinsProj01-WebApp/src/main/java/com/nt/servlet/WishMessageServlet.java
+++ JenkinsProj01-WebApp/src/main/java/com/nt/servlet/WishMessageServlet.java
@@ -19,7 +19,7 @@ public class WishMessageServlet extends HttpServlet {
 		//get PrintWriter
 		PrintWriter pw=resp.getWriter();
 		//write the message to response object
-		pw.println("<h1 style='color:red;text-align:center'>Hello Good Morning</h1>");
+		pw.println("<h1 style='color:red;text-align:center'>Hello Good Morning 1234</h1>");
 		//home hyperlink
 		pw.println("<br><br><a href='index.jsp'>home</a>");
 		//close the stream
